e1d15f8e911d4eb9949d0e2e2c3a75f6e1055abb
Ogua ahmed <ogua.ahmed18@gmail.com>
2024-07-06 22:02:44 +0000

    updated btn

--- resources/views/livewire/pos-component.blade.php
+++ resources/views/livewire/pos-component.blade.php
@@ -712,7 +712,7 @@ Gift card payment:
             <a href="{{ route('print-cash-register', $this->cash_register->id) }}"
                 target="_blank"
                 style="--c-400:var(--success-400);--c-500:var(--success-500);--c-600:var(--success-600);" class="fi-btn relative grid-flow-col items-center justify-center font-semibold outline-none transition duration-75 focus-visible:ring-2 rounded-lg fi-color-custom fi-btn-color-success fi-color-success fi-size-md fi-btn-size-md gap-1.5 px-3 py-2 text-sm inline-grid shadow-sm bg-custom-600 text-white hover:bg-custom-500 focus-visible:ring-custom-500/50 dark:bg-custom-500 dark:hover:bg-custom-400 dark:focus-visible:ring-custom-400/50">
-                Print fff
+                Print
             </a>
 
             @if ($this->registertype == "close")
