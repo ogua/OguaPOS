315d4a826529af2986ad6700b31c49be85491d9d
Ogua ahmed <ogua.ahmed18@gmail.com>
2024-07-06 22:01:40 +0000

    updated

--- resources/views/livewire/pos-component.blade.php
+++ resources/views/livewire/pos-component.blade.php
@@ -708,10 +708,12 @@ Gift card payment:
     
     <x-slot name="footer">
         <div class="flex gap-x-5">
-
-            <x-filament::button color="success" icon="heroicon-o-receipt-percent" outline>
-                Print
-            </x-filament::button>
+            
+            <a href="{{ route('print-cash-register', $this->cash_register->id) }}"
+                target="_blank"
+                style="--c-400:var(--success-400);--c-500:var(--success-500);--c-600:var(--success-600);" class="fi-btn relative grid-flow-col items-center justify-center font-semibold outline-none transition duration-75 focus-visible:ring-2 rounded-lg fi-color-custom fi-btn-color-success fi-color-success fi-size-md fi-btn-size-md gap-1.5 px-3 py-2 text-sm inline-grid shadow-sm bg-custom-600 text-white hover:bg-custom-500 focus-visible:ring-custom-500/50 dark:bg-custom-500 dark:hover:bg-custom-400 dark:focus-visible:ring-custom-400/50">
+                Print fff
+            </a>
 
             @if ($this->registertype == "close")
 
