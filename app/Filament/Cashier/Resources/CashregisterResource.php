61ae0e368b196df2bd20f4f332624d10ecf7b44e
Ogua ahmed <ogua.ahmed18@gmail.com>
2024-07-06 10:22:48 +0000

    updated register:

--- app/Filament/Cashier/Resources/CashregisterResource.php
+++ app/Filament/Cashier/Resources/CashregisterResource.php
@@ -105,7 +105,8 @@ class CashregisterResource extends Resource
                 //
             ])
             ->actions([
-                Tables\Actions\EditAction::make(),
+               // Tables\Actions\EditAction::make(),
+                Tables\Actions\DeleteAction::make(),
             ])
             ->bulkActions([
                 Tables\Actions\BulkActionGroup::make([
